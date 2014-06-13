2a68606f73a94e23014c05a95cf9ed3cb3ee4043
R.V.d.M <rvdm@RVdMs-MacBook-Pro.local>
2014-06-12 19:14:04 -0700

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function( grunt ) {
+
+  grunt.initConfig({
+
+    watch: {
+
+      files: 'app/*',
+
+      options: {
+        livereload: true,
+      },
+
+    },
+
+    connect: {
+
+      server: {
+
+        options: {
+          base: 'app',
+          open: true,
+          livereload: true
+        },
+
+      },
+
+    },
+
+  });
+
+  grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.registerTask( 'default' , [
+
+    'connect',
+    'watch'
+
+  ]);
+
+};
